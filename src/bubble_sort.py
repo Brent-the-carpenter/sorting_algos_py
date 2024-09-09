44d1f85c52e61e6b93bf6a5bc56a1f28b0c08e0a
Brent-the-carpenter <beh337@gmail.com>
2024-09-08 22:35:14 -0400

    added sudo code instructions.

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,3 +1,17 @@
+
+# steps:
+# Set swapping to True
+# Set end to the length of the input list
+# While swapping is True:
+# Set swapping to False
+# For i from the 2nd element to end:
+# If the (i-1)th element of the input list is greater than the ith element:
+# Swap the (i-1)th element and the ith element
+# Set swapping to True
+# Decrement end by one
+# Return the sorted list
+
+
 def bubble_sort(nums):
     print("hello")
 
--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,3 +1,12 @@
+# Complete the insertion_sort function according to the given pseudocode:
+
+# For each index in the input list:
+# Set a j variable to the current index
+# While j is greater than 0 and the element at index j-1 is greater than the element at index j:
+# Swap the elements at indices j and j-1
+# Decrement j by 1
+# Return the list
+
 
 def insertion_sort():
     print("hello")
